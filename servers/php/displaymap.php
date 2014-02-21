5b30fba0e9edc78e6fa593f9edb0c75477d1874d
Nick Fox <nicolaspatrickfox@hotmail.com>
2014-02-20 23:34:14 -0800

    switched to jquery 1.11.0 for compatibility with older versions of IE

--- servers/php/displaymap.php
+++ servers/php/displaymap.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
 	
     <script src="https://maps.googleapis.com/maps/api/js?v=3&sensor=false&libraries=adsense"></script>
-    <script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="javascript/maps.js"></script>
     <link href="styles/styles.css" rel="stylesheet" type="text/css" />
 
