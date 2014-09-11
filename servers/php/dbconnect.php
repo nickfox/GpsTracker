5ecc67e3b359b6ecdb5f600e8c83741eb458a01f
Nick Fox <nicolaspatrickfox@hotmail.com>
2014-09-10 18:24:12 -0700

    changed db

--- servers/php/dbconnect.php
+++ servers/php/dbconnect.php
@@ -6,7 +6,7 @@ $params = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_EMULATE_PREPARES => false, 
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
 
-$pdo = new PDO('mysql:host=localhost;dbname=gpstracker2;charset=utf8', $dbuser, $dbpass, $params);
+$pdo = new PDO('mysql:host=localhost;dbname=gpstracker;charset=utf8', $dbuser, $dbpass, $params);
 
 if (version_compare(PHP_VERSION, '5.3.6', '<')) {
     $pdo->exec('set names utf8');
