963ef09dcc73f3cc2c5b446b99b27c0948de995b
Nick Fox <nicolaspatrickfox@hotmail.com>
2014-11-23 23:02:31 -0800

    added replace function for decimals in updatelocation, thanks Wim

--- servers/php/updatelocation.php
+++ servers/php/updatelocation.php
@@ -1,11 +1,14 @@
 <?php
     include 'dbconnect.php';
     
-    $latitude       = isset($_GET['latitude']) ? $_GET['latitude'] : 0;
-    $longitude      = isset($_GET['longitude']) ? $_GET['longitude'] : 0;
+    $latitude       = isset($_GET['latitude']) ? $_GET['latitude'] : '0';
+    $latitude       = (float)str_replace(",", ".", $latitude); // to handle European locale decimals
+    $longitude      = isset($_GET['longitude']) ? $_GET['longitude'] : '0';
+    $longitude      = (float)str_replace(",", ".", $longitude);    
     $speed          = isset($_GET['speed']) ? $_GET['speed'] : 0;
     $direction      = isset($_GET['direction']) ? $_GET['direction'] : 0;
-    $distance       = isset($_GET['distance']) ? $_GET['distance'] : 0;
+    $distance       = isset($_GET['distance']) ? $_GET['distance'] : '0';
+    $distance       = (float)str_replace(",", ".", $distance);
     $date           = isset($_GET['date']) ? $_GET['date'] : '0000-00-00 00:00:00';
     $date           = urldecode($date);
     $locationmethod = isset($_GET['locationmethod']) ? $_GET['locationmethod'] : '';
